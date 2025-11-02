9882482b1c9767e53fc91254cff43720f3d760a0
Derin <derinciba08@gmail.com>
2025-11-02 10:33:13 +0000

    02/11/2025 10:33

--- ProgressBytes/Progress.py
+++ ProgressBytes/Progress.py
@@ -1,4 +1,1 @@
-character = int(input("What is your character? ")) #takes input and stores into variable
-characterAscii = int(ord(character))  #turns into binary data
-print(characterAscii) #prints input as ninary data
 
